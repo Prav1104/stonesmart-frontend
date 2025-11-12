eefccda9932aa9f24ecf573d5ca4e3e6a5f5eb50
Praveen Nitharwal <praveennitharwal55@gmail.com>
2025-11-12 16:35:54 +0530

    modified api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://stonesmart-backend.onrender.com/api", // later change to Render URL after deployment
+  baseURL: "https://stonesmart-backend.onrender.com", 
 });
 
 export default api;
